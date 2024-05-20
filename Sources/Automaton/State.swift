3f72dba13b8d6cdcbecd8e4ffc3aecc787ae7838
NutanNiraula <nutanniraula@gmail.com>
2024-05-20 13:05:24 +0700

    Add error description

--- Sources/Automaton/State.swift
+++ Sources/Automaton/State.swift
@@ -5,8 +5,19 @@
 //  Created by Nutan Niraula on 2/6/23.
 //
 
+import Foundation
+
 public protocol State: CaseIterable, Hashable, RawRepresentable, Equatable {}
 
 public enum StateError: Error, Equatable {
     case impossible(String)
+}
+
+extension StateError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .impossible(let reason):
+            return NSLocalizedString(reason, comment: "")
+        }
+    }
 }
